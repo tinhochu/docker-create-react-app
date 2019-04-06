debefe407f3426e15a0c120a5b9e6111a70318dd
Tin Chu <me@tinho.co>
2019-04-06 12:58:32 -0500

    Added Code to make the Server serve de client React Application
    Signed-off-by: Tin Chu <me@tinho.co>

--- server/index.js
+++ server/index.js
@@ -3,12 +3,12 @@ const mongoose = require('mongoose')
 const cookieSession = require('cookie-session')
 const passport = require('passport')
 const bodyParser = require('body-parser')
-const keys = require('./config/keys')
-require('./models/User')
-require('./models/Survey')
-require('./services/passport')
-
-mongoose.connect(keys.mongoURI)
+// const keys = require('./config/keys')
+// require('./models/User')
+// require('./models/Survey')
+// require('./services/passport')
+//
+// mongoose.connect(keys.mongoURI)
 
 const app = express()
 
@@ -22,9 +22,9 @@ app.use(
 app.use(passport.initialize())
 app.use(passport.session())
 
-require('./routes/authRoutes')(app)
-require('./routes/billingRoutes')(app)
-require('./routes/surveyRoutes')(app)
+// require('./routes/authRoutes')(app)
+// require('./routes/billingRoutes')(app)
+// require('./routes/surveyRoutes')(app)
 
 if (process.env.NODE_ENV === 'production') {
   // Express will serve up production assets
